2a5561c93e5050e6c0f25d6b7ba24bfb13f57031
Yancy Xerxes <704597765@qq.com>
2016-03-02 17:39:28 +0800

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,82 +1,48 @@
-/***********堆排序*************/
+/**********快速排序***********/
 
 #include <stdio.h>
 #define SIZE 5
 
-void heapsort(int array[],int heapsize);
-void max_heapify(int array[],int i,int heapsize);   //维护最大堆性质
-void build_max_heap(int array[],int heapsize);      //建堆
-int parent(int i);            //计算父结点下标
-int lchild(int i);              //计算左孩子结点下标
-int rchild(int i);             //计算右孩子结点下标
+int partition(int array[],int p,int q);         //p到q之间划分区间，返回pivot所在位置的下标
+void quicksort(int array[],int p,int q);     //快速排序
+void swap(int *a,int *b);                       //交换元素值
 
 int main(){
-    int i;
-    int array[SIZE];
-    printf("Please input the %d numbers : ",SIZE);
-    for(i=0;i<SIZE;i++){
+    int i,array[SIZE];
+    printf("Input %d numbers:",SIZE);
+    for(i=0;i<SIZE;i++)
         scanf("%d",&array[i]);
-    }
-    heapsort(array,SIZE);       //堆排序
-
-    printf("After the heapsort: ");
-    for(i=0;i<SIZE;i++){
-        printf("%d  ",array[i]);
-    }
+    quicksort(array,0,SIZE-1);
+    printf("After quicksort: ");
+    for(i=0;i<SIZE;i++)
+        printf("%d ",array[i]);
     return 0;
 }
 
-void heapsort(int array[],int heapsize){
-    int i,max;
-    build_max_heap(array,heapsize);
-    for(i=heapsize-1;i>=0;i--){
-        max =  array[0];
-        array[0]  = array[i];
-        array[i] = max;
-        max_heapify(array,0,i);
+int partition(int array[],int p,int q){
+    int pivot = array[q];
+    int pivot_position;
+    int i=p,j=p-1;
+    for(i=p;i<q;i++){
+        if(array[i]<=pivot){
+            j++;
+            swap(&array[i],&array[j]);
+        }
+    }
+    swap(&(array[q]),&(array[j+1]));
+    return (j+1);
+}
+
+void quicksort(int array[],int p,int q){
+    if(p<q){
+        int r = partition(array,p,q);
+        quicksort(array,0,r-1);         //递归实现quicksort
+        quicksort(array,r+1,q);
     }
 }
 
-
-void max_heapify(int array[],int i,int heapsize){
-    int lch = lchild(i);          //左孩子下标
-    int rch = rchild(i);          //右孩子下标
-    int max = 0;
-    int swap = -1;              //交换用的中间变量
-
-    if(lch<heapsize&&array[lch]>array[i])
-        max = lch;
-    else
-        max = i;
-
-    if(rch<heapsize&&array[rch]>array[i])
-        max = rch;
-
-    if(max != i){
-        swap = array[max];
-        array[max] = array[i];
-        array[i] = swap;
-        max_heapify(array,max,heapsize);
-    }
-
-}
-
-
-void build_max_heap(int array[],int heapsize){
-    int i = parent(heapsize-1);     //起点的下标
-    for(i;i>=0;i--)
-        max_heapify(array,i,heapsize);
-}
-
-
-int parent(int i){
-    return ((i-1)/2);
-}
-
-int lchild(int i){
-    return (i*2+1);
-}
-
-int rchild(int i){
-    return (i*2+2);
+void swap(int* a,int* b){
+    int c= *a;
+    *a = *b;
+    *b = c;
 }
